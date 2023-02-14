70b8118a487867cb86a7ad9d2408dee1c615be0b
Y Sussman <ypaulsussman@gmail.com>
2023-02-14 12:38:36 -0600

    add editable MokkogenMokko

--- src/Mokkogen.jsx
+++ src/Mokkogen.jsx
@@ -1,38 +1,47 @@
-import React, { useEffect, useState } from "react";
-// import { useNavigate, useParams } from "react-router-dom";
+import React, { useCallback, useEffect, useState } from "react";
 import { db } from "./data/db";
-import { MOKKOGEN_COMPLETE } from "./data/constants";
+import { INITIAL_MOKKO_DATA, MOKKOGEN_COMPLETE } from "./data/constants";
 import { getRandomArrayIndex, isBuiltInCueNote } from "./utils/noteUtils";
+import { validateMokko } from "./utils/mokkoUtils";
 import MokkogenNote from "./components/MokkogenNote";
+import MokkogenMokko from "./components/MokkogenMokko";
 
 function Mokkogen() {
   const [baseNote, setBaseNote] = useState();
   const [cueNote, setCueNote] = useState();
-  // const [currentMokko, setCurrentMokko] = useState();
+  const [newMokko, setNewMokko] = useState({
+    ...INITIAL_MOKKO_DATA,
+    base_note_id: null,
+    cue_note_id: null,
+    date_created: new Date().toISOString().slice(0, 10),
+  });
   const [mokkogenStage, setMokkogenStage] = useState(1);
+
+  const getBaseNote = useCallback(async function () {
+    const todayString = new Date().toISOString().slice(0, 10);
+    const allEligibleBaseNotes = await db.notes
+      .filter(
+        ({ suspended, cue_type, next_occurrence }) =>
+          !suspended &&
+          !isBuiltInCueNote(cue_type) &&
+          next_occurrence <= todayString
+      )
+      .toArray();
+
+    if (!allEligibleBaseNotes) {
+      setBaseNote(MOKKOGEN_COMPLETE);
+    } else {
+      const randomizerOffset = getRandomArrayIndex(allEligibleBaseNotes.length);
+      setBaseNote(allEligibleBaseNotes[randomizerOffset]);
+      setNewMokko({
+        ...newMokko,
+        base_note_id: allEligibleBaseNotes[randomizerOffset].id,
+      });
+    }
+  }, []);
 
   // load baseNote
   useEffect(() => {
-    async function getBaseNote() {
-      const todayString = new Date().toISOString().slice(0, 10);
-      const allEligibleBaseNotes = await db.notes
-        .filter(
-          ({ suspended, cue_type, next_occurrence }) =>
-            !suspended &&
-            !isBuiltInCueNote(cue_type) &&
-            next_occurrence <= todayString
-        )
-        .toArray();
-
-      if (!allEligibleBaseNotes) {
-        setBaseNote(MOKKOGEN_COMPLETE);
-      } else {
-        const randomizerOffset = getRandomArrayIndex(
-          allEligibleBaseNotes.length
-        );
-        setBaseNote(allEligibleBaseNotes[randomizerOffset]);
-      }
-    }
     getBaseNote();
   }, []);
 
@@ -57,20 +66,55 @@ function Mokkogen() {
         .toArray();
 
       const randomizerOffset = getRandomArrayIndex(allEligibleCueNotes.length);
+      setNewMokko({
+        ...newMokko,
+        cue_note_id: allEligibleCueNotes[randomizerOffset].id,
+      });
       setCueNote(allEligibleCueNotes[randomizerOffset]);
     }
 
     getCueNote(baseNote.id, baseNote.allowed_cue_types);
   }, [baseNote?.id, baseNote?.allowed_cue_types]);
 
-  const handleBaseNoteUpdate = (newText) => {
+  const handleBaseNoteContentUpdate = (newText) => {
     // NB future Y: see same weird issue in <EditNote>
     setBaseNote((baseNote) => ({ ...baseNote, content: newText }));
   };
 
-  const handleCueNoteUpdate = (newText) => {
+  const handleCueNoteContentUpdate = (newText) => {
     // NB future Y: see same weird issue in <EditNote>
     setCueNote((cueNote) => ({ ...cueNote, content: newText }));
+  };
+
+  const handleNewMokkoContentUpdate = (newText) => {
+    // NB future Y: see same weird issue in <EditNote>
+    setNewMokko((newMokko) => ({ ...newMokko, content: newText }));
+  };
+
+  const handleMokkoSubmit = async (e) => {
+    e.preventDefault();
+
+    const { validatedMokko, validationErrors } = validateMokko(newMokko);
+    if (validationErrors) {
+      alert(validationErrors);
+      return;
+    } else {
+      await db.mokkos.add(validatedMokko);
+      await db.notes.update(baseNote.id, {
+        prior_mokkogen_interactions: [
+          ...baseNote.prior_mokkogen_interactions,
+          cueNote.id,
+        ],
+      });
+      await db.notes.update(cueNote.id, {
+        prior_mokkogen_interactions: [
+          ...cueNote.prior_mokkogen_interactions,
+          baseNote.id,
+        ],
+      });
+      getBaseNote();
+      setMokkogenStage(1);
+    }
   };
 
   if (!baseNote) {
@@ -86,7 +130,7 @@ function Mokkogen() {
       <div className="flex flex-wrap justify-center gap-8 m-12">
         <MokkogenNote
           note={baseNote}
-          setNote={handleBaseNoteUpdate}
+          setNote={handleBaseNoteContentUpdate}
           mokkogenStage={mokkogenStage}
           setMokkogenStage={setMokkogenStage}
           isCue={false}
@@ -94,10 +138,17 @@ function Mokkogen() {
         {[2, 3].includes(mokkogenStage) && cueNote && (
           <MokkogenNote
             note={cueNote}
-            setNote={handleCueNoteUpdate}
+            setNote={handleCueNoteContentUpdate}
             mokkogenStage={mokkogenStage}
             setMokkogenStage={setMokkogenStage}
             isCue={true}
+          />
+        )}
+        {mokkogenStage === 3 && (
+          <MokkogenMokko
+            newMokko={newMokko}
+            setNewMokko={handleNewMokkoContentUpdate}
+            handleMokkoSubmit={handleMokkoSubmit}
           />
         )}
       </div>
--- src/components/MokkogenMokko.jsx
+++ src/components/MokkogenMokko.jsx
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
+import RichTextEditor from "./RichTextEditor";
 
-function MokkogenMokko(params) {
-  return <>someday I'ma be a real component!</>;
+function MokkogenMokko({ newMokko, setNewMokko, handleMokkoSubmit }) {
+  return (
+    <div className="card card-bordered shadow-lg shadow-gray-500 prose">
+      <div className="card-body flex-col justify-between">
+        <RichTextEditor
+          currentText={newMokko.content}
+          handleContentUpdate={setNewMokko}
+        />
+        <div className="flex justify-end mt-4">
+          <button className="btn btn-ghost" onClick={handleMokkoSubmit}>
+            Save Mokko
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MokkogenMokko;
