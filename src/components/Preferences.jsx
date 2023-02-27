ea6fbc48eebd7b806e122e932a598261d9222438
Y Sussman <ypaulsussman@gmail.com>
2023-02-27 13:25:40 -0600

    make daily-note-limit adjustable

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -1,32 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { getUserPreferences } from "../utils/appUtils";
 import { db } from "../data/db";
 
 function Preferences() {
-  // eslint-disable-next-line no-unused-vars
-  const [userPreferences, setUserPreferences] = useState()
+  const [userPreferences, setUserPreferences] = useState();
   const [prefersDarkMode, setPrefersDarkMode] = useState(
     () =>
       window.matchMedia &&
       window.matchMedia("(prefers-color-scheme: dark)").matches
   );
 
-  useEffect(() => {
-    async function loadPreferences() {
-      const {
-        prefersDarkMode: appSpecificDarkModePreference,
-        ...otherUserPreferences
-      } = await getUserPreferences();
+  const loadPreferences = useCallback(async function () {
+    const {
+      prefersDarkMode: appSpecificDarkModePreference,
+      ...otherUserPreferences
+    } = await getUserPreferences();
 
-      // If the user hasn't set it, defer to browser/os
-      if (appSpecificDarkModePreference !== null) {
-        setPrefersDarkMode(appSpecificDarkModePreference);
-      }
-
-      setUserPreferences(otherUserPreferences)
+    // If the user hasn't set it, defer to browser/os
+    if (appSpecificDarkModePreference !== null) {
+      setPrefersDarkMode(appSpecificDarkModePreference);
     }
-    loadPreferences();
+
+    setUserPreferences(otherUserPreferences);
   }, []);
+
+  // initial dataload
+  useEffect(() => {
+    loadPreferences();
+  }, [loadPreferences]);
 
   const toggleDarkMode = async () => {
     const newDarkModePreference = !prefersDarkMode;
@@ -40,21 +41,52 @@ function Preferences() {
     setPrefersDarkMode(newDarkModePreference);
   };
 
-  return (
-    <div>
-      <div className="form-control">
-        <label className="label cursor-pointer">
-          <span className="label-text">Toggle Dark Mode</span>
+  const handleDailyLimitChange = async ({ target: { value } }) => {
+    console.log("value: ", value);
+    const newDailyLimit = value ? Number(value) : 0;
+    await db.preferences.toCollection().modify((preferences) => {
+      preferences.mokkogenDailyLimit.ceiling = newDailyLimit;
+      return;
+    });
+    await loadPreferences();
+  };
+
+  if (!userPreferences) {
+    return <></>;
+  } else {
+    return (
+      <div>
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <span className="label-text">Toggle Dark Mode</span>
+            <input
+              type="checkbox"
+              className="toggle"
+              onChange={toggleDarkMode}
+              checked={prefersDarkMode}
+            />
+          </label>
+        </div>
+
+        <div className="form-control mt-8">
+          <label className="label">
+            <span className="label-text">Maximum daily notes for mokkogen:</span>
+          </label>
           <input
-            type="checkbox"
-            className="toggle"
-            onChange={toggleDarkMode}
-            checked={prefersDarkMode}
+            type="text"
+            className="input input-bordered"
+            value={userPreferences.mokkogenDailyLimit.ceiling}
+            onChange={handleDailyLimitChange}
           />
-        </label>
+          <label className="label">
+            <span className="label-text-alt">
+              Leave blank or set to &quot;0&quot; to remove daily limit
+            </span>
+          </label>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Preferences;
