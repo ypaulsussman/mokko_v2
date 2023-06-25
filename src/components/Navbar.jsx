d5ecae913827ebef9df39d992817e7011d9604ee
Y Sussman <ypaulsussman@gmail.com>
2023-06-25 15:10:15 -0500

    update deps && daisyUI navbar syntax

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,18 @@ function Navbar() {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <Link className="p-2 bg-base-100" to={`/`}>
-          Home
-        </Link>
-        <Link className="ml-2 p-2 bg-base-100" to={`about`}>
-          About
-        </Link>
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link className="p-2 bg-base-100" to={`/`}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="ml-2 p-2 bg-base-100" to={`about`}>
+              About
+            </Link>
+          </li>
+        </ul>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
@@ -18,29 +24,20 @@ function Navbar() {
             <Link to={`mokkogen`}>Mokkogen</Link>
           </li>
           <li tabIndex={0}>
-            <a>
-              Manage
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="z-10 p-4 bg-base-100 shadow-lg shadow-gray-500">
-              <li>
-                <Link to={`manage/notes`}>Notes</Link>
-              </li>
-              <li>
-                <Link to={`manage/mokkos`}>Mokkos</Link>
-              </li>
-              <li>
-                <Link to={`manage/settings`}>Settings</Link>
-              </li>
-            </ul>
+            <details>
+              <summary>Manage</summary>
+              <ul className="z-10 p-4 bg-base-100 shadow-lg shadow-gray-500">
+                <li>
+                  <Link to={`manage/notes`}>Notes</Link>
+                </li>
+                <li>
+                  <Link to={`manage/mokkos`}>Mokkos</Link>
+                </li>
+                <li>
+                  <Link to={`manage/settings`}>Settings</Link>
+                </li>
+              </ul>
+            </details>
           </li>
         </ul>
       </div>
